766a20b787e3dda306a81713e7f88315e095a207
Kenneth Soriano <Kennethsoriano14017@gmail.com>
2023-02-03 11:44:03 -0800

    Global colors and styling

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ import AddPlace from './screens/AddPlace';
 import Map from './screens/Map';
 import PlaceDetails from './screens/PlaceDetails';
 import IconButton from './components/UI/IconButton';
+import { Colors } from './constants/colors';
 
 const Stack = createNativeStackNavigator();
 
@@ -17,11 +18,16 @@ export default function App() {
     <>
       <StatusBar style='dark' />
       <NavigationContainer>
-        <Stack.Navigator>
+        <Stack.Navigator screenOptions={{
+          headerStyle: { backgroundColor: Colors.primary500 },
+          headerTintColor: Colors.gray700,
+          contentStyle: { backgroundColor: Colors.gray700}
+        }}>
           <Stack.Screen 
             name="AllPlaces"
             component={AllPlaces}
             options={({navigation}) => ({
+              title: 'Your Favorite Places',
               headerRight: ({tintColor}) => (
                 <IconButton 
                   icon="add" 
@@ -35,6 +41,9 @@ export default function App() {
           <Stack.Screen 
             name="AddPlace"
             component={AddPlace}
+            options={{
+              title: 'Add a new Place',
+            }}
           />
           <Stack.Screen 
             name="Map"
