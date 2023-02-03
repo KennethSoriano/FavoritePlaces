94367019f87d9a7ee25c8266dbfbbd23a777e0a7
Kenneth Soriano <Kennethsoriano14017@gmail.com>
2023-02-03 09:49:58 -0800

    Adding a header button

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ import AllPlaces from './screens/AllPlaces';
 import AddPlace from './screens/AddPlace';
 import Map from './screens/Map';
 import PlaceDetails from './screens/PlaceDetails';
+import IconButton from './components/UI/IconButton';
 
 const Stack = createNativeStackNavigator();
 
@@ -20,6 +21,16 @@ export default function App() {
           <Stack.Screen 
             name="AllPlaces"
             component={AllPlaces}
+            options={({navigation}) => ({
+              headerRight: ({tintColor}) => (
+                <IconButton 
+                  icon="add" 
+                  size={24} 
+                  color={tintColor} 
+                  onPress={() => navigation.navigate('AddPlace')}
+                />
+              ),
+            })}
           />
           <Stack.Screen 
             name="AddPlace"
